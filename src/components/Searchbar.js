c75087852df1f766a815ba53169f1578daea596f
Joshua Martin <joshua@bluefishds.com>
2018-04-27 14:02:49 -0400

    Destructure search params better

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { updateQuery, updateSort } from './../actions/search';
 
-let Searchbar = ({ search: { query, filters, sort }, updateQuery, updateSort }) =>
+let Searchbar = ({ query, filters, sort, updateQuery, updateSort }) =>
   <form>
     <input type="search" placeholder="Search" value={query} onChange={(e) => updateQuery(e.target.value)} />
     <select value={sort} onChange={(e) => updateSort(e.target.value)}>
@@ -11,7 +11,7 @@ let Searchbar = ({ search: { query, filters, sort }, updateQuery, updateSort }) 
     </select>
   </form>
 
-let mapStateToProps = ({ search }) => ({ search });
+let mapStateToProps = ({ search }) => ({ ...search });
 
 let mapDispatchToProps = {
   updateQuery,
