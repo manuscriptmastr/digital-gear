16275bf69ac49dbe5dd3e11494d7c04688d46540
Joshua Martin <joshua@bluefishds.com>
2018-05-02 18:37:48 -0400

    CategoryScreen complete

--- src/components/CategoryScreen.js
+++ src/components/CategoryScreen.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import Layout from './Layout';
-import CategoryList from './CategoryList';
+import Category from './Category';
+import ProductList from './ProductList';
+import { updateProducts } from './../actions/products';
+import { updateCategories } from './../actions/categories';
+import { fetchProducts } from './../api/products';
+import { fetchCategories } from './../api/categories';
 import { connect } from 'react-redux';
+import { compose, withHandlers, lifecycle } from 'recompose';
+import { fromCategory, pipe } from './../lib/utils';
 
-let CategoryScreen = ({ categories }) =>
+let CategoryScreen = ({ products, categories }) =>
   <Layout>
-    <CategoryList categories={categories}/>
+    {
+      categories.map(c =>
+        <div>
+          <Category category={c}/>
+          <ProductList products={pipe(fromCategory(c._id))(products)} />
+        </div>
+      )
+    }
   </Layout>
 
-let mapStateToProps = ({ categories }) => ({ categories });
+let mapStateToProps = ({ products, categories, jwt }) => ({ products, categories, jwt });
 
-export default connect(mapStateToProps)(CategoryScreen);
+let mapDispatchToProps = {
+  updateProducts,
+  updateCategories
+};
+
+let enhance = compose(
+  connect(mapStateToProps, mapDispatchToProps),
+  withHandlers({
+    updateProducts: ({ jwt, updateProducts }) => async () => {
+      let products = await fetchProducts(jwt);
+      updateProducts(products);
+    },
+    updateCategories: ({ jwt, updateCategories }) => async () => {
+      let categories = await fetchCategories(jwt);
+      updateCategories(categories);
+    }
+  }),
+  lifecycle({
+    componentDidMount() {
+      this.props.updateProducts();
+      this.props.updateCategories();
+    }
+  })
+);
+
+export default enhance(CategoryScreen);
