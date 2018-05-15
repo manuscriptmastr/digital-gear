16b25671715ca5f066027239d724925238e2f35b
Joshua Martin <joshua@bluefishds.com>
2018-05-15 09:54:47 -0400

    Adjust avatar

--- src/components/Product.js
+++ src/components/Product.js
@@ -2,9 +2,9 @@ import React from 'react';
 import './Product.css';
 import { Link } from 'react-router-dom';
 
-let Product = ({ product: { _id, price, title, images } }) =>
+let Product = ({ product: { _id, price, title, images: [ img ] } }) =>
   <Link className="product" to={`/products/${_id}`}>
-    <img className="product-image" src={images[0].url} alt={images[0].title} />
+    <img className="product-image" src={img.url} alt={img.title} />
     <h1 className="product-title">{title} - ${price}</h1>
   </Link>
 
